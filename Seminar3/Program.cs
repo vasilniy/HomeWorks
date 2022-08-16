77e37d1d431f8f926ea4fe3e524f519ba4cb12ac
vasiliy <vasilniy@rambler.ru>
2022-08-16 21:12:11 +0600

    Задача 23. Число в кубе

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -65,5 +65,14 @@ Console.Write($"Расстояние между точками = {FindDistance3d
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+/*
+void NumberCubed(int N)
+{
+    for( int i = 1; i <= N; i++)     
+        Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");    
+}
 
-
+Console.Write("Веедите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NumberCubed(number);
+*/
