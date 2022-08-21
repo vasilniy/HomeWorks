d063c392f1f69c96167fdf103edaa9b1731d19d1
vasiliy <vasilniy@rambler.ru>
2022-08-21 23:43:38 +0600

    ДЗ к семинару №5 решена задача №34

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -2,7 +2,45 @@
 // трёхзначными числами. Напишите программу, которая покажет количество
 // чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+    for (int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
 
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+int FindEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) 
+        {
+            count++;
+            Console.WriteLine($"{count} - {array[i]}");// Для удобства сверки :)
+        }
+    return count;
+}
+
+
+Console.Write("Введи размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи мин. значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи макс. значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(a, min, max);
+ShowArray(myArray);
+Console.WriteLine($"Количество четных числе в массиве =  {FindEvenNumbers(myArray)}");
+*/
 
 
 
