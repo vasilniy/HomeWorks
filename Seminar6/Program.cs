2b4846843bab4414dbad23c4ba4dd1fe653474c0
vasiliy <vasilniy@rambler.ru>
2022-08-24 23:25:51 +0600

    ДЗ к семинару №6. Плохо решена Задача №43, не понимаю решения, если проверять, когда прямые не пересекатся, в итоге задача без метода

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -2,43 +2,61 @@
 // больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
-
-void ShowArray(int[] array)
+/*
+void ShowArray(double[] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (double i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
-int[] FillArray(int M)
+double[] FillArray(double M)
 {
-    int[] array = new int[M];
-    for (int i = 0; i < array.Length; i++)
+    double[] array = new double[M];
+    for (double i = 0; i < array.Length; i++)
     {
         Console.Write($"Введи {i + 1} - число: ");
-        array[i] = Convert.ToInt32(Console.ReadLine());
+        array[i] = Convert.Todouble32(Console.ReadLine());
     }
 
     return array;
 }
-int NumberCounter(int[] array)
+double NumberCounter(double[] array)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    double count = 0;
+    for (double i = 0; i < array.Length; i++)
     {
         if (array[i] > 0) count++;
     }
 
     return count;
 }
-/*
+
 Console.Write("Введи сколько будет чисел: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int[] MyArray = FillArray(num);
+double num = Convert.Todouble32(Console.ReadLine());
+double[] MyArray = FillArray(num);
 ShowArray(MyArray);
 Console.WriteLine($"Количество чисел больше нуля: {NumberCounter(MyArray)}");
 */
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1, 
+// y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+// Уж простите, Паавел, но эта задача без метода.
+//
 
+Console.Write("Введи b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введи k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введи b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введи k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k1*x + b1;
+Console.WriteLine($"Точка пересечения: ( {x} ; {y} )");
