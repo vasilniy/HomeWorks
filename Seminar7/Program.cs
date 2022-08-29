54a7198f4a3b464061155d18c9bce447c151a993
vasiliy <vasilniy@rambler.ru>
2022-08-29 21:21:27 +0600

    ДЗ к семинару №7. Решена задача №47

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -5,13 +5,44 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double[,] CreateRandom2dArray(int rows, int columns, int minVolume, int maxVolume)
+{
+    double[,] newArray = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().NextDouble() * (new Random().Next(minVolume, maxVolume + 1));
 
+    return newArray;
+}
+void Show2dArray(double[,] Array) // вывод массива с двумя знаками после запятой
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            if (Array[i,j] < 0) Console.Write("{0}\t",Math.Round(Array[i, j], 2));
+            else Console.Write("{0}\t"," " + Math.Round(Array[i, j], 2));
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введи количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine()); 
+
+double[,] MyArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(MyArray);
 
 
 
 
 // Задача 50. Напишите программу, которая на вход принимает
-// позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// позиции элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
