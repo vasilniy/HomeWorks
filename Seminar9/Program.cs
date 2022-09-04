3037e9be3a8070622881068942ae7e80eeff3022
vasiliy <vasilniy@rambler.ru>
2022-09-04 22:39:28 +0600

    ДЗ к Семинару №9. Решена задача №68.

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -29,4 +29,17 @@ Console.WriteLine(SumOfNaturalNumbers(m, n));
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 29
 
+/*
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return Akkerman(m - 1, 1);
 
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int A = Akkerman (3 ,2);
+Console.WriteLine(A);
+*/
